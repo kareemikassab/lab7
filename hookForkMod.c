19e88b245d363d58c48d75fc58aee299a89d2b1e
kareemikassab <kareemikassab@aucegypt.edu>
2021-03-29 01:29:22 +0200

    lab7

--- hookForkMod.c
+++ hookForkMod.c
@@ -7,12 +7,13 @@
 #include <asm/segment.h>
 #include <linux/buffer_head.h>
 #include <linux/slab.h>
+#include <linux/kallsyms.h>
 MODULE_LICENSE("GPL");
 
 const char* FILEPATH="/proc/version";
 char MAPPATH[128] ="/boot/System.map-";
 
-typedef asmlinkage long (*sys_call_ptr_t)(unsigned long p1, void* p2, int* p3,int* p4, unsigned long p5);
+typedef asmlinkage long (*system_call_ptr_t)(unsigned long p1, void* p2, int* p3,int* p4, unsigned long p5);
 static sys_call_ptr_t clone_add;
 int countery = 0;
 sys_call_ptr_t* sysCall;
@@ -23,8 +24,11 @@ static asmlinkage long forkyy(unsigned long p1, void* p2, int* p3,int* p4, unsig
     
     countery=countery+1;
    printk("The fork called: %d \n", countery);
-    
-    return clone_add (p1, p2, p3, p4, p5);
+
+long trial;
+//return 
+	trial= clone_add(p1, p2, p3, p4, p5);
+return trial;
 }
 
 struct myfile{
@@ -34,7 +38,7 @@ loff_t pos;
 };
 
 
-
+/*
 struct myfile* open_file (const char *filePath) 
 {
     struct myfile *mfptr;
@@ -45,8 +49,8 @@ struct myfile* open_file (const char *filePath)
     set_fs(mfptr->fs);
     return mfptr;
 }
-
-
+*/
+/*
 int read_from_file_until(struct myfile *mfptr, unsigned char* buf,unsigned long  vlen, char c) 
 {
    int ret;
@@ -62,13 +66,13 @@ int i;
 	}
     set_fs(mfptr->fs);
     return ret;
-}   
-
+} */  
+/*
 void close_file(struct myfile *myfileptr) 
 {
     filp_close(myfileptr->f, NULL);
 }
-
+*/
 
 inline void writerFunc(unsigned long holder2)
 {
@@ -91,55 +95,14 @@ static void enableWrite(void)
 
 
 int init_module(void){
-struct myfile *mfptr;
 
-mfptr= kmalloc(sizeof(struct myfile), GFP_KERNEL);
-mfptr= open_file(FILEPATH);
-
-	
-	
-	char* rbuffer;
-	rbuffer= kmalloc(sizeof(char),GFP_KERNEL);
-	mfptr->pos=0;
-	read_from_file_until(mfptr,rbuffer,1024, ' ');
-	read_from_file_until(mfptr,rbuffer,1024, ' ');
-	read_from_file_until(mfptr,rbuffer,1024, ' ');
-	printk(KERN_ALERT "Kernel_Version: %s\n", rbuffer);
-	close_file(mfptr);
-	
-	
-	strncat(MAPPATH, rbuffer, 20);
-	printk(KERN_ALERT "file: %s\n", MAPPATH);
-	mfptr=open_file(MAPPATH);
-	
-		
-		char* rbuffer2;
-		rbuffer2= kmalloc(sizeof(char),GFP_KERNEL);
-		mfptr->pos=0;
-		int i; 
-		while (true){
-		
-		read_from_file_until(mfptr,rbuffer2,1024, '\n');
-		if (strstr (rbuffer2, "sys_call_table")!= NULL)
-			break;	
-		}
-		char Address [17];
-		strncpy(Address,rbuffer2,16);
-		Address[16] ='\0';
-		printk(KERN_ALERT "FirstLineinsysmap: %s\n", rbuffer2);
-		printk(KERN_ALERT "FirstLineAddress: %s\n", Address);	
-		unsigned long holder; 
-		sscanf(Address,"%lx", &holder);
-		sysCall= (sys_call_ptr_t *) holder;
+		sysCall= kallsyms_lookup_name("sys_call_table");
 		printk(KERN_ALERT " Fork Address: %px \n", sysCall[2]);
 		clone_add = sysCall[__NR_clone];
     	enableWrite();
-    	sysCall[__NR_clone] = forkyy;
+    	sysCall[__NR_clone] = (sys_call_ptr_t)forkyy;
 		disableWrite();
 
-kfree(rbuffer2);
-kfree(mfptr);
-kfree(rbuffer);	
 return 0; 
 }
 
