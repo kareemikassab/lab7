6626278a423fd6fced4a0f535f394c72e783b106
kareemikassab <50639617+kareemikassab@users.noreply.github.com>
2021-03-28 23:33:56 +0200

    Add files via upload

--- Readme.txt
+++ Readme.txt
@@ -6,3 +6,7 @@ To run the code:
 sudo isnmod ./hookForkMod.ko
 tail var/log/kern.log
 sudo rmmod /hookForkMod.ko
+
+
+
+Note: I print everyfork for testing but i will change ut after
--- hookForkMod.c
+++ hookForkMod.c
@@ -5,13 +5,27 @@
 #include <linux/stat.h>
 #include <linux/fs.h>
 #include <asm/segment.h>
-#include <asm/uaccess.h>
 #include <linux/buffer_head.h>
 #include <linux/slab.h>
 MODULE_LICENSE("GPL");
 
 const char* FILEPATH="/proc/version";
 char MAPPATH[128] ="/boot/System.map-";
+
+typedef asmlinkage long (*sys_call_ptr_t)(unsigned long p1, void* p2, int* p3,int* p4, unsigned long p5);
+static sys_call_ptr_t clone_add;
+int countery = 0;
+sys_call_ptr_t* sysCall;
+
+
+static asmlinkage long forkyy(unsigned long p1, void* p2, int* p3,int* p4, unsigned long p5)
+{
+    
+    countery=countery+1;
+   printk("The fork called: %d \n", countery);
+    
+    return clone_add (p1, p2, p3, p4, p5);
+}
 
 struct myfile{
 struct file* f; 
@@ -55,6 +69,27 @@ void close_file(struct myfile *myfileptr)
     filp_close(myfileptr->f, NULL);
 }
 
+
+inline void writerFunc(unsigned long holder2)
+{
+    asm volatile("mov %0,%%cr0": "+r" (holder2), "+m" (__force_order));
+}
+
+static void disableWrite(void)
+{
+  unsigned long reg_cr0 = read_cr0();
+  set_bit(16, &reg_cr0);
+  writerFunc(reg_cr0);
+}
+
+static void enableWrite(void)
+{
+  unsigned long reg_cr0 = read_cr0();
+  clear_bit(16, &reg_cr0);
+  writerFunc(reg_cr0);
+} 
+
+
 int init_module(void){
 struct myfile *mfptr;
 
@@ -93,11 +128,14 @@ mfptr= open_file(FILEPATH);
 		Address[16] ='\0';
 		printk(KERN_ALERT "FirstLineinsysmap: %s\n", rbuffer2);
 		printk(KERN_ALERT "FirstLineAddress: %s\n", Address);	
-		sys_call_ptr_t* sysCall;
 		unsigned long holder; 
 		sscanf(Address,"%lx", &holder);
 		sysCall= (sys_call_ptr_t *) holder;
 		printk(KERN_ALERT " Fork Address: %px \n", sysCall[2]);
+		clone_add = sysCall[__NR_clone];
+    	enableWrite();
+    	sysCall[__NR_clone] = forkyy;
+		disableWrite();
 
 kfree(rbuffer2);
 kfree(mfptr);
@@ -106,6 +144,9 @@ return 0;
 }
 
 void cleanup_module(void){
+	enableWrite();
+    sysCall[__NR_clone] = clone_add;
+disableWrite();
 	printk(KERN_INFO " Tamam keda\n");
 }
 
